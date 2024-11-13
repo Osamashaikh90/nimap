6808ad1dd2ca2a9b3eced5a93f5c3f77e0c4bae1
Osama <shaikhosama499@gmail.com>
2024-11-13 23:43:35 +0530

    chore(components): alt added in images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ const basePath = import.meta.env.VITE_IMAGE_BASE_URL
 const rating  = (movie.vote_average).toFixed(1);
   return (
     <div className="relative w-56 overflow-hidden cursor-pointer rounded-xl">
-        <img src={basePath+movie?.poster_path} alt="whattt" className="rounded-xl"/>
+        <img src={basePath+movie?.poster_path} alt={movie.title} className="rounded-xl"/>
         <div className="w-full py-3 text-white ">
         <h3 className="text-sm font-bold hover:underline">{movie.title}</h3>
         <p className="mt-1 text-xs">Rating: {rating}</p>
